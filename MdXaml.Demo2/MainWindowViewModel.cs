4a7de7cf50a65e05685ab93eb04aa19e8809c541
whistyun <kanadische_goldrute@yahoo.co.jp>
2021-02-09 00:11:28 +0900

    add new demo for source property

---
+++ MdXaml.Demo2/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MdXaml.Demo2
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Uri _MdSource;
+        public Uri MdSource
+        {
+            get => _MdSource;
+            set
+            {
+                if (_MdSource == value) return;
+                _MdSource = value;
+                FirePropertyChanged();
+            }
+        }
+
+
+        /// <summary> <see cref="INotifyPropertyChanged"/> </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// <see cref="INotifyPropertyChanged"/>のイベント発火用
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void FirePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null && propertyName != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
