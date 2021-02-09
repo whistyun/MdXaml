59c100f1eef165030b5c28140da720a9f37afac0
whistyun <kanadische_goldrute@yahoo.co.jp>
2021-02-10 07:42:24 +0900

    update demo

--- MdXaml.Demo2/MainWindowViewModel.cs
+++ MdXaml.Demo2/MainWindowViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace MdXaml.Demo2
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private int ReferenceIdx = 0;
+
+        public ObservableCollection<Uri> Histories { get; } = new ObservableCollection<Uri>();
+
         private Uri _MdSource;
         public Uri MdSource
         {
@@ -16,7 +22,41 @@ namespace MdXaml.Demo2
             {
                 if (_MdSource == value) return;
                 _MdSource = value;
+
+                Histories.Add(value);
+                ReferenceIdx = Histories.Count - 1;
+
                 FirePropertyChanged();
+            }
+        }
+
+        public ICommand Prev { get; }
+        public ICommand Next { get; }
+
+        public MainWindowViewModel()
+        {
+            Prev = new Command(PrevPage);
+            Next = new Command(NextPage);
+
+            MdSource = new Uri("Assets/Main.md", UriKind.Relative);
+
+        }
+
+        public void NextPage()
+        {
+            if (ReferenceIdx < Histories.Count - 1)
+            {
+                _MdSource = Histories[++ReferenceIdx];
+                FirePropertyChanged(nameof(MdSource));
+            }
+        }
+
+        public void PrevPage()
+        {
+            if (ReferenceIdx > 0)
+            {
+                _MdSource = Histories[--ReferenceIdx];
+                FirePropertyChanged(nameof(MdSource));
             }
         }
 
@@ -36,5 +76,18 @@ namespace MdXaml.Demo2
                 PropertyChanged(this, e);
             }
         }
+    }
+
+    public class Command : ICommand
+    {
+        private Action Target;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action a) { Target = a; }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => Target.Invoke();
     }
 }
