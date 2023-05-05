3a7d87a570958e55607c010406c3642ae1601318
whistyun <kanadische_goldrute@yahoo.co.jp>
2023-05-05 11:23:26 +0900

    support animatedgif and image sizing; refs #60

---
+++ MdXaml.Plugins/IElementLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MdXaml.Plugins
+{
+    public interface IElementLoader
+    {
+        public FrameworkElement? Load(Stream stream);
+    }
+}
